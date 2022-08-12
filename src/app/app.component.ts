ef143b1d4daafc1b65c72db32d83c974a4b1f2c4
Sanjay Garg <sgarg5858@gmail.com>
2022-08-12 12:26:40 +0530

    implemented CVA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,11 +6,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'forms-in-angular';
 
   userForm=new FormGroup({
     name:new FormControl('',[Validators.required]),
-    age:new FormControl(24)
+    age:new FormControl({value:24,disabled:true},[Validators.required,Validators.min(18),Validators.max(60)])
   })
+  ngOnInit(): void {
+      
+  }
+
+  log()
+  {
+    console.log(this.userForm);
+  }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,26 +1,78 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
+  providers:[
+    {
+      provide:NG_VALUE_ACCESSOR,
+      multi:true,
+      useExisting:CounterComponent
+    }
+  ]
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent implements OnInit,ControlValueAccessor {
 
   @Input() change:number=1;
+
   counter:number=0;
+  touched:boolean=false;
+  disabled:boolean=false;
+
+  onChange = (value:number)=>{};
+  onTouch = ()=>{}
+
+  registerOnChange(fn: any): void {
+    this.onChange=fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch=fn;
+  }
+
   constructor() { }
+
+  writeValue(counter: number): void {
+    this.counter=counter;
+  }
+ 
+  setDisabledState(isDisabled:boolean)
+  {
+    this.disabled=isDisabled;
+  }
 
   ngOnInit(): void {
   }
 
   increment()
   {
-    this.counter=this.counter + this.change;
+    if(!this.disabled)
+    {
+      this.markAsTouched();
+      this.counter=this.counter + this.change;
+      this.onChange(this.counter);
+    }
   }
   decrement()
   {
+   if(!this.disabled)
+   {
+    this.markAsTouched();
     this.counter=this.counter - this.change;
+    this.onChange(this.counter);
+   }
+
+  }
+
+  markAsTouched()
+  {
+    console.log("HELLo")
+    if(!this.touched)
+    {
+      this.onTouch();
+      this.touched=true;
+    }
   }
 
 }
